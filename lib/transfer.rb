aa22ceedc07725562796a6afba859cb6772f5ea5
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-23 19:22:31 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,6 @@ class Transfer
   
   def execute_transaction
     if self.valid? && @status = "pending" 
-      binding.pry
       @sender.balance = (@sender.balance - @amount)
       @receiver.balance = (@receiver.balance + @amount)
       @status = "complete"
