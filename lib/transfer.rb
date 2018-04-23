221708e14c7caca82b746786ee48b8327d2a45f6
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-23 18:51:41 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
     if self.valid? 
       @sender.balance = (@sender.balance - @amount) 
       @receiver.balance = (@receiver.balance + @amount)
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
