648787df387c6b52b3bb4212f94620ed480945d9
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-23 19:39:45 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if self.status = "complete"
+    if status = "complete"
       receiver.balance -= amount
       sender.balance += amount
       status = "reversed"
